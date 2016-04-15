389ad89da4c04eede797ea269a2abbca98d25891
marianbuchly <marianbuchly@gmail.com>
2016-04-15 10:10:12 +0200

    nieuw

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,12 +8,19 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  include Cloudinary::CarrierWave
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumbnail do
+    eager
+    resize_to_fit(300, 300)
+    cloudinary_transformation :quality => 80
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
